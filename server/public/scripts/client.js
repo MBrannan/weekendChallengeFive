add9bde7288e45436080e2b914a7150a9c175e12
Michael Brannan <michaelbrannan@Michaels-MacBook-Pro.local>
2016-11-28 17:44:23 -0600

    95% MVP

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,49 @@
-console.log("pls");
+var employeeApp = angular.module('calculator', ['ngRoute']);
+
+employeeApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/employees', {
+      templateUrl: '/views/templates/employees.html',
+      controller: 'EmployeeController',
+      controllerAs: 'employees'
+    })
+    .otherwise({
+      redirectTo: 'employees'
+    });
+}]);
+
+employeeApp.controller('EmployeeController', ['$http', function($http) {
+  var self = this;
+  self.monthlySalary = 0;
+  console.log('Employee controller loaded');
+  self.employees = [];
+  self.newEmployee = {};
+  getEmployees();
+  calculateSalary();
+
+  function getEmployees() {
+    $http.get('/employees')
+      .then(function(response) {
+        console.log(response.data);
+        self.employees = response.data;
+      });
+  }
+
+  function calculateSalary() {
+    self.monthlySalary += Math.round(self.employees.salary / 12); //This is returning NaN despite salary being a number?
+    console.log(self.monthlySalary);
+  }
+
+  self.addEmployee = function(newEmployee) {
+      $http.post('/employees', newEmployee)
+       .then(function(response) {
+         console.log('employee added: ', response);
+         self.employees.push(angular.copy(self.newEmployee));
+         getEmployees();
+       });
+    }
+  self.deleteEmployee = function(deleteTarget) {
+    $http.delete('/employees/' + deleteTarget.id)
+     .then(getEmployees);
+  };
+}]);
--- server/routes/employees.js
+++ server/routes/employees.js
@@ -5,6 +5,71 @@ var connectionString = 'postgres://localhost:5432/employees';
 
 router.get('/', function(req, res) {
   console.log('get request');
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM employees', function (err, result) {
+      done();
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+      console.log(result.rows);
+    });
+  });
+});
+
+router.post('/', function(req, res) {
+  var newEmployee = req.body;
+  console.log('post request');
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error ', err);
+      res.sendStatus(500);
+    }
+    console.log(newEmployee);
+    client.query('INSERT INTO employees (first_name, last_name, idNumber, title, salary)' +
+      'VALUES ($1, $2, $3, $4, $5)',
+      [newEmployee.first_name, newEmployee.last_name, newEmployee.idNumber, newEmployee.title, newEmployee.salary],
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.log('insert query error: ', err)
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+    });
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('DELETE FROM employees WHERE id = $1',
+      [req.params.id],
+      function(err, result) {
+        done();
+        if (err) {
+          console.log('deletion error: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      }
+    );
+  });
 });
 
 module.exports = router;
